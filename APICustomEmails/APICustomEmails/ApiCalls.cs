0eaea7f33447976ccf5180508c982bb45035d63d
Cylindric <mark@hanfordonline.co.uk>
2017-02-23 14:01:22 +0000

    Initial commit

---
+++ APICustomEmails/APICustomEmails/ApiCalls.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Reflection;
+
+namespace APICustomEmails
+{
+    public class ApiCalls
+    {
+        //Declare Variables
+        private string Username { get; set; }
+        private string Password { get; set; }
+        private string InstanceName { get; set; }
+        private string Email1xml;
+        private string Email2xml;
+
+
+        private API.SDK _api;
+
+
+
+        //Set username, password and instance name. Set URL for API commands
+        public ApiCalls(string username, string password, string instancename)
+        {
+            _api = new API.SDK();
+
+            Username = username;
+            Password = password;
+            InstanceName = instancename;
+
+            _api.AuthHeaderValue = new API.AuthHeader()
+            {
+                Username = this.Username,
+                Password = this.Password
+            };
+
+            _api.Url = $"http://www.communigatormail.co.uk/{InstanceName}/SDK.asmx";
+        }
+
+
+
+
+        //Preload XML files. Use OuterXml as it needs to be in string format
+        public void LoadXML()
+        {
+
+            var rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            try
+            {
+                XmlDocument xml1 = new XmlDocument();
+                xml1.Load(Path.Combine(rootPath, @"Email1.xml"));
+                Email1xml = xml1.OuterXml;
+
+                XmlDocument xml2 = new XmlDocument();
+                xml2.Load(Path.Combine(rootPath, @"Email2.xml"));
+                Email2xml = xml2.OuterXml;
+                
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Warning! XML files not found! Ensure that ExistingCustomer.xml and NewCustomer.xml are present in exe folder");
+                Console.WriteLine("Please press enter to try again");
+                Console.ReadLine();
+                LoadXML();
+            }
+
+        }
+
+
+
+
+        //////////////////////////////////////////////////////////////////Begin API call list
+
+
+        //Authentication Check
+        public string AuthenticationCheck()
+        {
+            var result = _api.AuthenticationCheck();
+            return result;
+        }
+
+
+        //Send email 1
+        public string SendEmail1()
+        {
+            var result = _api.UploadContactTriggerCampaign(Email1xml);
+            return result;
+        }
+    
+        //Send email 2
+        public string SendEmail2()
+        {
+            var result = _api.UploadContactTriggerCampaign(Email2xml);
+            return result;
+        }
+
+        //Return Campaign Stats
+        public string ReturnCampaignStats(int id)
+        {
+            var result = _api.CampaignStats(id);
+
+            return result.ToString();
+        }
+
+        //Return sent emails
+        public string ReturnSentEmails()
+        {
+            var result = _api.ReturnSentEmails();
+
+            return result.ToString();
+        }
+
+        //Reset counter
+        public string ResetCounter()
+        {
+            var result = _api.ResetCounter("ReturnSentEmails");
+
+            return result.ToString();
+        }
+    }
+}
---
+++ APICustomEmails/APICustomEmails/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICustomEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   ////SET THESE PARAMETERS FROM THE SDK USERNAME (INSTANCE ENDS IN LZ). YOU CAN ACCESS THIS IN THE FRONT END OF COMMUNIGATOR UNDER TOOLS-> INTEGRATION AND THEN THE SECURITY TAB
+            var api = new ApiCalls("USERNAME", "PASSWORD", "INSTANCENAME");
+
+
+         
+                Console.WriteLine("Preloading XML files");
+                api.LoadXML();
+            
+          
+                Console.WriteLine("XML Files Loaded");
+                Console.WriteLine("\n");
+
+                int choice = 0;
+                Console.WriteLine("Welcome to the CommuniGator API test app, please select one of the following options (press 7 to quit):");
+
+                do
+                {
+
+                    Console.WriteLine("\n");
+                    Console.WriteLine("/////////////////////////////////////");
+                    Console.WriteLine("1. Perform authentication check");
+                    Console.WriteLine("2. Send email 1");
+                    Console.WriteLine("3. Send email 2");
+                    Console.WriteLine("/////////////////////////////////////");
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Examples of other API calls (for use with tracked links/opens from sent emails)");
+                    Console.WriteLine("/////////////////////////////////////");
+                    Console.WriteLine("4. Return campaign stats");
+                    Console.WriteLine("5. Return sent emails");
+                    Console.WriteLine("6. Reset counter for above");
+                    Console.WriteLine("/////////////////////////////////////");
+                    var ans = Console.ReadLine();
+
+
+                    choice = InputHandler(api, ans);
+                } while (choice != 7);
+            
+            
+        }
+
+
+
+
+        private static int InputHandler(ApiCalls api, string ans)
+        {
+            int choice;
+            if (int.TryParse(ans, out choice))
+            {
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine(api.AuthenticationCheck());
+                        break;
+
+                    case 2:
+                        Console.WriteLine(api.SendEmail1());
+                        break;
+
+                    case 3:
+                        Console.WriteLine(api.SendEmail2());
+                        break;
+
+
+                    case 4:
+                        Console.WriteLine("Please enter the campaign id:");
+                        string id1 = Console.ReadLine();
+                        int id = 0;
+                        int.TryParse(id1, out id);
+                        Console.WriteLine(api.ReturnCampaignStats(id));
+                        break;
+
+                    case 5:
+                        Console.WriteLine(api.ReturnSentEmails());
+                        break;
+
+                    case 6:
+                        Console.WriteLine(api.ResetCounter());
+                        break;
+
+                    case 7:
+                        break;
+
+                }
+            }
+
+            return choice;
+        }
+    }
+}
